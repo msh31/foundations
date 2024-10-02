5e9506814d741b08d974125081316d20aa511adc
Marco <mshendriks03@gmail.com>
2024-10-02 10:14:07 +0200

    finished excersise 6 npc names

--- npc names/Program.cs
+++ npc names/Program.cs
@@ -14,3 +14,42 @@ string RandomConsonant() {
     string consonant = consonants[index];     
     return consonant;
 }
+
+string MaleName()
+{
+    string name = RandomConsonant();
+
+    for (int i = 0; i < 5; i++)
+    {
+ //if i / 2 is 0 add a vowel to the name otherwise a consonent
+        if (i % 2 == 0) { name += RandomVowel(); }
+        else { name += RandomConsonant(); }
+    }
+    return name;
+}
+
+string FemaleName()
+{
+    string name = RandomVowel();
+    for (int i = 0; i < 5; i++)
+    {
+        if (i % 2 == 0) { name += RandomConsonant(); }
+        else { name += RandomVowel(); }
+    }
+    return name;
+}
+
+void ListOfMaleNames(int amount)
+{
+    Console.WriteLine("Male names:");
+    for (int i = 0; i < amount; i++) { Console.WriteLine($"{i + 1}. {MaleName()}"); }
+}
+
+void ListOfFemaleNames(int amount)
+{
+    Console.WriteLine("Female names:");
+    for (int i = 0; i < amount; i++) { Console.WriteLine($"{i + 1}. {FemaleName()}"); }
+}
+
+ListOfMaleNames(10);
+ListOfFemaleNames(10);
